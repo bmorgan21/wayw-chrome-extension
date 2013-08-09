007c2aaec3a1f009267fc4ce7d7c5224cdbf315b
Brian Morgan <brian.s.morgan@gmail.com>
2013-08-08 23:46:21 -0400

    initial commit

---
+++ js/background.js
@@ -0,0 +1,88 @@
+var NOTIFICATION_ID = 'wayw';
+
+chrome.browserAction.setBadgeBackgroundColor({'color': '#0000FF'});
+chrome.browserAction.setBadgeText({text: "?"});
+
+var next_check_time = new Date();
+var delay_seconds = function(seconds) {
+    next_check_time = new Date();
+    next_check_time.setSeconds(next_check_time.getSeconds() + seconds);
+}
+
+console.log('aaa');
+chrome.runtime.onInstalled.addListener(function() {
+    console.log("Installed.");
+
+    setInterval(check_status, 30*1000); // 30 seconds
+    check_status();
+
+    chrome.notifications.onButtonClicked.addListener(function(notificationId, buttonIndex) {
+        if (notificationId == NOTIFICATION_ID) {
+            delay_seconds(30*60); // 30 minutes
+
+            if (buttonIndex == 0) {
+                var url = chrome.extension.getURL('popup.html');
+                var options = 'width=325,height=325,top=200,left=450';
+                window.popUpWindow = window.open(url, "Where Are You Working?", options);
+            }
+
+            chrome.notifications.clear(notificationId, function(wasCleared) {
+            });
+        }
+    });
+
+    chrome.notifications.onClosed.addListener(function(notificationId, byUser) {
+        if (notificationId == NOTIFICATION_ID && byUser) {
+            delay_seconds(5*60); // 5 minutes
+            chrome.notifications.clear(notificationId, function(wasCleared) {
+            });
+        }
+    });
+});
+
+console.log('bbb');
+function check_status() {
+    var now = new Date();
+
+    year = now.getFullYear();
+    month = now.getMonth() + 1;
+    if (month < 10) {
+        month = '0' + month;
+    }
+    day = now.getDate();
+    if (day < 10) {
+        day = '0' + day;
+    }
+
+    var date_str = year + '-' + month + '-' + day;
+    var key = 'date-' + date_str;
+
+    if (key in localStorage) {
+        chrome.browserAction.setBadgeBackgroundColor({'color': '#00FF00'});
+        chrome.browserAction.setBadgeText({text: "ok"});
+    } else {
+        chrome.browserAction.setBadgeBackgroundColor({'color': '#FF0000'});
+        chrome.browserAction.setBadgeText({text: "X"});
+
+        if (now.getHours() > 9 && now > next_check_time) {
+            var opt = {
+                type: "basic",
+                title: "Where Are You Working?",
+                message: "Please provide your location details.",
+                iconUrl: "images/icon.png",
+                buttons: [{title:'Report Now', iconUrl:'images/pencil.png'},
+                          {title:'Ask Me Again Later', iconUrl:'images/clock.png'}]
+            }
+
+            console.log('show notification');
+            // chrome.notifications.clear(NOTIFICATION_ID, function(wasCleared) {
+            //     console.log('cleared ' + wasCleared);
+            // });
+            chrome.notifications.create(NOTIFICATION_ID, opt, function(notificationId) {
+                console.log('create ' + notificationId);
+            });
+        }
+    }
+}
+
+console.log('ccc');
---
+++ js/popup.js
@@ -0,0 +1,116 @@
+var format_date = function(date_str) {
+    return moment(date_str + ' 00:00').format('dddd, MMMM DD, YYYY');
+}
+
+var update_date = function(date_str, $di, $dt) {
+    $di.attr('value', date_str);
+    $dt.html(format_date(date_str));
+}
+
+var check_email = function($email, init) {
+    var $help_block = $email.next('.help-block');
+
+    if ($.trim($email.val()) == '') {
+        $help_block.html('please provide an email address');
+        $email.closest('div').addClass('has-error');
+        if (!init) {
+            $email.closest('div').removeClass('init');
+        }
+        return false;
+    }
+
+    return true;
+}
+
+var check_location = function($well, $location) {
+    var $help_block = $well.find('.help-block');
+
+    if (!$location.val()) {
+        $well.addClass('has-error');
+        $help_block.html('please select a location');
+        return false;
+    }
+
+    return true;
+}
+
+var day_of_week = function(date_str) {
+    return moment(date_str + ' 00:00').format('dddd')
+}
+
+$(document).ready(function(){
+    var $dp = $('#dp');
+    var $dt = $dp.find('.text');
+    var $di = $dp.next('input[name="d"]');
+
+    $dp.data('date', moment().format('YYYY-MM-DD'));
+    $dp.datepicker()
+		.on('changeDate', function(ev){
+			$dp.datepicker('hide');
+            update_date($dp.data('date'), $di, $dt);
+		});
+    update_date($dp.data('date'), $di, $dt);
+
+    var $form = $('#f');
+    var $email = $form.find('input[name="e"]');
+    var $date = $form.find('input[name="d"]');
+    var $well = $form.find('.well');
+    var $locations = $form.find('input[name="l"]');
+
+    $form.find('input[name="l"][value="' + localStorage['location-' + day_of_week($dp.data('date'))] + '"]').prop('checked', true);
+
+    $email.val(localStorage.email);
+    check_email($email, true);
+
+    $form.on('submit', function(evt) {
+        var $location = $form.find('input[name="l"]:checked');
+
+        if (!check_email($email, false)) return false;
+        if (!check_location($well, $location)) return false;
+
+        var email = $email.val();
+        var date = $date.val();
+        var location = $location.val();
+
+        localStorage.email = email;
+        localStorage['date-' + date] = true;
+        localStorage['location-' + day_of_week($date.val())] = location;
+
+        $.post('http://photatom.com/wyaw/?e=' + encodeURIComponent(email), {a:'update', d:date, l:location}, function(data) {
+            var $panel_footer = $('.panel-footer');
+
+            if (data.indexOf('OK') == 0) {
+                $('#myModal .modal-footer').hide();
+                $('#myModal .modal-title').html('Confirm');
+                $('#myModal .modal-body').html('Updated Successfully! [' + location + ' on ' + date + ']');
+                $('#myModal').modal('show');
+                setTimeout(window.close, 2*1000);
+                var bp = chrome.extension.getBackgroundPage();
+                bp.check_status();
+            } else {
+                $('#myModal .modal-footer').show();
+                $('#myModal .modal-title').html('Error');
+                $('#myModal .modal-body').html(data);
+                $('#myModal').modal('show');
+            }
+        });
+
+        return false;
+    });
+
+    $email.on('focus', function(evt) {
+        $email.closest('div').removeClass('has-error');
+    });
+
+    $locations.on('change', function(evt) {
+        $locations.closest('div').removeClass('has-error');
+    });
+
+    var $btn_later = $('button.later');
+    $btn_later.on('click', function(evt) {
+        var bp = chrome.extension.getBackgroundPage();
+        bp.delay_seconds(30*60); // 30 minutes
+        window.close();
+        return false;
+    });
+});
